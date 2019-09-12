92c3d5e8c1a72ba0408e67aa70999784da5e8763
Marcin Bartminski <mbdjj@o2.pl>
2019-09-12 19:59:02 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Marcin Bartminski on 12/09/2019.
+//  Copyright © 2019 Marcin Bartminski. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done:Bool = false
+}
